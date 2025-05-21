0612bd1bb5b7acf14f37434baf7dd4b2963d3895
Andrea <dea0312@gmail.com>
2025-05-21 15:16:38 -0300

    fix: corrigir erro de importação do global.css no build

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,3 +6,9 @@ declare module '*.module.css' {
 //Isso diz ao TypeScript como lidar com CSS Modules.
 // Ele informa ao TypeScript que quando você importar um arquivo CSS Module, ele deve tratar isso como um objeto onde as chaves são strings e os valores também são strings.
 // Isso é útil para evitar erros de tipo ao usar CSS Modules em seu código TypeScript.
+
+declare module '*.css';
+
+//Esse arquivo declara que qualquer importação de .css é um módulo válido, o que resolve o erro TS2307.
+
+// Isso é útil para evitar erros de tipo ao usar arquivos CSS em seu código TypeScript.
